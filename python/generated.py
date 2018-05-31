d0b5766eb0262970f5977625540b8a7b47276f23
Lipin Pius <Lipin-Pius@github.com>
2018-05-31 10:57:23 +0530

    latest

--- python/generated.py
+++ python/generated.py
@@ -1,13 +1,24 @@
 import tensorflow as tf
 
+
 node = amazon-s3.storage.amazon-s3(node)
+
 node = tf.layers.conv2d(filters=32, kernel_size=(3,3), activation=tf.nn.relu, input_shape=,node)
+
 node = tf.layers.conv2d(filters=64, kernel_size=(3,3), activation=tf.nn.relu, input_shape=,node)
+
 node = tf.layers.maxpooling2d(pool_size=(2,2),node)
+
 node = tf.layers.dropout(rate=0.25,node)
+
 node = tf.layers.flatten(node)
+
 node = tf.layers.dense(units=128, activation=tf.nn.relu,node)
+
 node = tf.layers.dropout(rate=0.5,node)
+
 node = tf.layers.dense(units=10, activation=tf.nn.softmax,node)
+
 node = tf.train.AdamOptimizer.adam(node)
+
 node = tf.losses.categorical_crossentropy(node)
--- python/pytemplates/python.py
+++ python/pytemplates/python.py
@@ -1,13 +1,5 @@
 import tensorflow as tf
 
-{{ data.0 }}
-{{ data.1 }}
-{{ data.2 }}
-{{ data.3 }}
-{{ data.4 }}
-{{ data.5 }}
-{{ data.6 }}
-{{ data.7 }}
-{{ data.8 }}
-{{ data.9 }}
-{{ data.10 }}
+{% for item in data.keys() %}
+{{ data[item] }}
+{% endfor %}
